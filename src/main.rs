3c3cf7f73898aaa8c0ce1d1b4589a2a7d7a79c5b
hmatt1 <hornung.m1@gmail.com>
2020-12-21 15:39:57 -0600

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 mod pieces;
+mod board;
 
-use pieces::*;
 use bevy::prelude::*;
-use std::fs;
+use bevy_mod_picking::*;
+use pieces::*;
+use board::*;
 
 fn main() {
 
-    let paths = fs::read_dir("./assets").unwrap();
-
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
-    }
     App::build()
         // Set antialiasing to use 4 samples
         .add_resource(Msaa { samples: 4 })
@@ -22,6 +19,7 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PickingPlugin)
         .add_startup_system(setup.system())
         .add_startup_system(create_board.system())
         .add_startup_system(create_pieces.system())
@@ -42,6 +40,7 @@ fn setup(
             )),
             ..Default::default()
         })
+        .with(PickSource::default())
 
         // Light
         .spawn(LightComponents {
@@ -50,181 +49,6 @@ fn setup(
         });
 }
 
-fn create_board (
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
-) {
-    // Add meshes and materials
-    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1. }));
-    let white_material = materials.add(Color::rgb(0.9, 1.0, 0.9).into());
-    let black_material = materials.add(Color::rgb(0., 0.2, 0.2).into());
 
 
-    // Spawn 64 squares
-    for i in 0..8 {
-        for j in 0..8 {
-            commands.spawn(PbrComponents {
-                mesh: mesh.clone(),
-                // Change material according to position to get alternating pattern
-                material: if (i + j + 1) % 2 == 0 {
-                    white_material.clone()
-                } else {
-                    black_material.clone()
-                },
-                transform: Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
-                ..Default::default()
-            });
-        }
-    }
-}
 
-fn create_pieces(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Load all the meshes
-    let king_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh0/Primitive0");
-    let king_cross_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh1/Primitive0");
-    let pawn_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh2/Primitive0");
-    let knight_1_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh3/Primitive0");
-    let knight_2_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh4/Primitive0");
-    let rook_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh5/Primitive0");
-    let bishop_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh6/Primitive0");
-    let queen_handle: Handle<Mesh> =
-        asset_server.load("pieces.glb#Mesh7/Primitive0");
-
-    // Add some materials
-    let white_material = materials.add(Color::rgb(0.9, 1.0, 0.9).into());
-    let black_material = materials.add(Color::rgb(0., 0.2, 0.2).into());
-
-    spawn_rook(
-        &mut commands,
-        white_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(0., 0., 0.),
-    );
-    spawn_knight(
-        &mut commands,
-        white_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(0., 0., 1.),
-    );
-    spawn_bishop(
-        &mut commands,
-        white_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(0., 0., 2.),
-    );
-    spawn_queen(
-        &mut commands,
-        white_material.clone(),
-        queen_handle.clone(),
-        Vec3::new(0., 0., 3.),
-    );
-    spawn_king(
-        &mut commands,
-        white_material.clone(),
-        king_handle.clone(),
-        king_cross_handle.clone(),
-        Vec3::new(0., 0., 4.),
-    );
-    spawn_bishop(
-        &mut commands,
-        white_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(0., 0., 5.),
-    );
-    spawn_knight(
-        &mut commands,
-        white_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(0., 0., 6.),
-    );
-    spawn_rook(
-        &mut commands,
-        white_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(0., 0., 7.),
-    );
-
-    for i in 0..8 {
-        spawn_pawn(
-            &mut commands,
-            white_material.clone(),
-            pawn_handle.clone(),
-            Vec3::new(1., 0., i as f32),
-        );
-    }
-
-    spawn_rook(
-        &mut commands,
-        black_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(7., 0., 0.),
-    );
-    spawn_knight(
-        &mut commands,
-        black_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(7., 0., 1.),
-    );
-    spawn_bishop(
-        &mut commands,
-        black_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(7., 0., 2.),
-    );
-    spawn_queen(
-        &mut commands,
-        black_material.clone(),
-        queen_handle.clone(),
-        Vec3::new(7., 0., 3.),
-    );
-    spawn_king(
-        &mut commands,
-        black_material.clone(),
-        king_handle.clone(),
-        king_cross_handle.clone(),
-        Vec3::new(7., 0., 4.),
-    );
-    spawn_bishop(
-        &mut commands,
-        black_material.clone(),
-        bishop_handle.clone(),
-        Vec3::new(7., 0., 5.),
-    );
-    spawn_knight(
-        &mut commands,
-        black_material.clone(),
-        knight_1_handle.clone(),
-        knight_2_handle.clone(),
-        Vec3::new(7., 0., 6.),
-    );
-    spawn_rook(
-        &mut commands,
-        black_material.clone(),
-        rook_handle.clone(),
-        Vec3::new(7., 0., 7.),
-    );
-
-    for i in 0..8 {
-        spawn_pawn(
-            &mut commands,
-            black_material.clone(),
-            pawn_handle.clone(),
-            Vec3::new(6., 0., i as f32),
-        );
-    }
-}
